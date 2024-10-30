11162c635d6300747c388b8cf141779bcddff70b
Sujan Basnet <77416492+Sujanbas@users.noreply.github.com>
2024-10-30 16:40:04 -0500

    Add files via upload

---
+++ partials/sidebar.php
@@ -0,0 +1,14 @@
+        <div class="sidebar">
+            <div class="profile">
+                <h1>IMS</h1>
+                <img src="./pics/user.jfif" alt="User Image">
+                <p>Hello 
+                <?= $user['first_name']?>
+                </p>
+            </div>
+            <ul>
+            <li><a href="adminDashboard.php" class=""active"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li><a href="#" class="active" ><i class="fas fa-users-cog"></i> User Management</a></li>
+            </ul>
+           
+         </div>
